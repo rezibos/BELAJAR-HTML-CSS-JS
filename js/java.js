4ef91f55178402ac7b8aeebde259c8edf44a02d5
rezibos <rezibos>
2025-01-11 21:30:09 +0700

    html

---
+++ js/java.js
@@ -0,0 +1,29 @@
+console.log('Hello World');
+
+//variables
+var name = 'John';
+let age = 30;
+console.log(age);
+age = 25;
+console.log(age);
+const pi = 3.14;
+console.log(pi);
+
+const array = [1,2,3,4,5];
+console.log(array);
+
+const Islogin = true;
+console.log(Islogin);
+
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+}
+console.log(person);
+
+const peaple = [
+    {name: 'John', age: 30, city: 'New York'},
+    {name: 'Jane', age: 25, city: 'New York'}
+]
+console.table(peaple);
